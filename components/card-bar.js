e3201dcc7585449a31653dce25a23a77ed8daf5f
jpmbdll <jpmbdll@gmail.com>
2024-03-07 17:22:23 +0800

    responsive text

--- components/card-bar.js
+++ components/card-bar.js
@@ -5,16 +5,24 @@ import CardWithChart from "./bar-chart";
 export default function CardBar({ id, title, sd, averages }) {
   return (
     <Card key={id} boxShadow="lg">
-      <CardHeader as="h3" size="md" fontStyle="italic">
+      <CardHeader
+        as="h1"
+        fontSize={{ base: "sm", md: "md" }}
+        fontStyle="italic"
+      >
         {title}
       </CardHeader>
       <CardBody pt={0}>
         <CardWithChart averages={averages} />
         <Flex justifyContent="flex-end" flexDirection="row">
-          <Text fontSize="md" pr={2}>
+          <Text fontSize={{ base: "sm", md: "md" }} pr={2}>
             Standard Deviation:
           </Text>
-          <Text fontSize="lg" fontStyle="italic" fontWeight="bold">
+          <Text
+            fontSize={{ base: "md", md: "lg" }}
+            fontStyle="italic"
+            fontWeight="bold"
+          >
             {sd}
           </Text>
         </Flex>
