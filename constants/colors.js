6c52a0a029a9e151978ed12744ccd4e6270a38b3
jpmbdll <jpmbdll@gmail.com>
2024-04-15 21:53:45 +0800

    change bar colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,1 +1,2 @@
-export const colors = ["#913831", "#4A0404", "#986868", "#954535", "#6E260E"];
+// export const colors = ["#913831", "#4A0404", "#986868", "#954535", "#6E260E"];
+export const colors = ["#FC023B", "#E9FC02", "#FF6C00", "#00FF13", "#00ECFF"];
